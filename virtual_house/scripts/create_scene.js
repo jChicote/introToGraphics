5052b4c9fb6d3ca7963d8b6b781307f8f99d4a07
Jaiden Chicote <jedezzio@gmail.com>
2019-04-29 20:51:53 +1000

    working changes but is unclean
    code hasnt been cleaned of some mistakes

--- virtual_house/scripts/create_scene.js
+++ virtual_house/scripts/create_scene.js
@@ -1,1 +1,31 @@
 
+function ClearScene(){
+  for (let i = scene.children.length - 1; i >= 0; i--)
+      if(scene.children[i].type == "Mesh")
+          scene.remove(scene.children[i]);
+}
+
+function CreateScene() {
+
+  //creating the floow
+  var geoFloor = new THREE.BoxBufferGeometry( 30, 0.1, 30 );
+	materialFloor = new THREE.MeshStandardMaterial( { color: 0x808080, roughness: 0, metalness: 0 } );
+	var floor = new THREE.Mesh( geoFloor, materialFloor );
+	scene.add( floor )
+
+  var sphere_color = new THREE.Color(0.8,1,1);
+  var sphere_geometry = new THREE.SphereGeometry(2, 32, 32 );
+  sphere_material = new THREE.MeshPhongMaterial();
+  sphere_material.color=sphere_color;
+  sphere_material.shininess=100;
+
+
+  sphere_material.wireframe=false;
+  sphere_mesh = new THREE.Mesh( sphere_geometry, sphere_material );
+  sphere_mesh.castShadow = true;
+  scene.add( sphere_mesh );
+
+  var ambient = new THREE.AmbientLight( 0xffffff, 1 );
+  scene.add( ambient );
+
+}
--- virtual_house/scripts/gui.js
+++ virtual_house/scripts/gui.js
@@ -1,10 +1,39 @@
 
 
-function gui_interface() {
-  var gui = new dat.GUI( {width: 300} );
+/*function buildGui() {
+  console.log(materialFloor);
+  console.log(materialFloor.color.getHex());
+  gui = new dat.GUI();
   gui.open();
 
-  var params = {
+  //gui parameters
+  var param = {
+    color: materialFloor.color.getHex()
+  }
 
-  };
+  //Folders
+  var flooring = gui.addFolder( 'Flooring' );
+
+  //gui functions
+  flooring.gui.addColor( param, 'color' ).onChange( function ( val ) {
+    console.log(val);
+    materialFloor.color.setHex( val );
+  } );
+
+}*/
+
+function buildGui() {
+      gui = new dat.GUI();
+      var params = {
+        color: materialFloor.color.getHex(),
+
+      }
+
+      var flooring = gui.addFolder( 'Flooring' );
+      flooring.addColor( params, 'color' ).onChange( function ( val ) {
+        materialFloor.color.setHex( val );
+      } );
+
+
+      gui.open();
 }
