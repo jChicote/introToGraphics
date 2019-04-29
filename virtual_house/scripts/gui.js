e4e45a608dd25d083cabc9d02c47e717e9272733
Jaiden Chicote <jedezzio@gmail.com>
2019-04-29 14:12:41 +1000

    Changes to gui,lighting and main

--- virtual_house/scripts/gui.js
+++ virtual_house/scripts/gui.js
@@ -1,1 +1,10 @@
 
+
+function gui_interface() {
+  var gui = new dat.GUI( {width: 300} );
+  gui.open();
+
+  var params = {
+
+  };
+}
--- virtual_house/scripts/lighting_script.js
+++ virtual_house/scripts/lighting_script.js
@@ -1,1 +1,20 @@
 //js lighting script
+
+function sunLight() {
+  /*This is only used for the creation of sunlight using pointlight
+
+  This takes x,y,z and orientation/rotation of light.
+  This also includes the specialisated code that allows for 
+  gui control of the light source. (IF NECESSARY) */
+
+
+}
+
+function objLight() {
+  /*This function is used for the creation of certain objects
+     that contain their own light source (lamps, etc).
+
+     It takes the x,y,z position/vector and also the orientation
+     of object*/
+
+}
