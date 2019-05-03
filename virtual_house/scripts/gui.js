a5dc672aae6a753cbdf780e016d743d86e750bb4
Jaiden Chicote <jedezzio@gmail.com>
2019-05-04 01:14:20 +1000

    Cleaned code presentaition and implemented basic light statements

--- virtual_house/scripts/gui.js
+++ virtual_house/scripts/gui.js
@@ -1,39 +1,30 @@
 
-
-/*function buildGui() {
-  console.log(materialFloor);
-  console.log(materialFloor.color.getHex());
-  gui = new dat.GUI();
-  gui.open();
-
-  //gui parameters
-  var param = {
-    color: materialFloor.color.getHex()
-  }
-
-  //Folders
-  var flooring = gui.addFolder( 'Flooring' );
-
-  //gui functions
-  flooring.gui.addColor( param, 'color' ).onChange( function ( val ) {
-    console.log(val);
-    materialFloor.color.setHex( val );
-  } );
-
-}*/
+  ////////////
+	// CUSTOM //
+	////////////
 
 function buildGui() {
       gui = new dat.GUI();
       var params = {
         color: materialFloor.color.getHex(),
+        'sunX': sunLight.position.x,
 
       }
 
       var flooring = gui.addFolder( 'Flooring' );
+      var lighting = gui.addFolder( 'Lighting' );
+
+      //Flooring Folder
       flooring.addColor( params, 'color' ).onChange( function ( val ) {
         materialFloor.color.setHex( val );
       } );
 
+      // Lighting Folder
+      var sunlightX = lighting.add( params, 'sunX' ).min(-200).max(200).step(1).listen();
+
+      sunlightX.onChange(function(value){
+        sunLight.position.x = value;
+      });
 
       gui.open();
 }
--- virtual_house/scripts/lighting_script.js
+++ virtual_house/scripts/lighting_script.js
@@ -4,9 +4,12 @@ function sunLight() {
   /*This is only used for the creation of sunlight using pointlight
 
   This takes x,y,z and orientation/rotation of light.
-  This also includes the specialisated code that allows for 
+  This also includes the specialisated code that allows for
   gui control of the light source. (IF NECESSARY) */
 
+  sunLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+  sunLight.position.set(0, 50, 0);
+  scene.add(sunLight, sunLight.target);
 
 }
 
