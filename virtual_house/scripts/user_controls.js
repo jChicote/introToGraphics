399eb6ff6f4692d94718e90651cc8f28cd0e1fe5
yuming2887 <49386276+yuming2887@users.noreply.github.com>
2019-06-03 13:16:18 +1000

    Update user_controls.js

--- virtual_house/scripts/user_controls.js
+++ virtual_house/scripts/user_controls.js
@@ -103,6 +103,65 @@ function UserControls() {
     }
   };
 
+/*  //camera, control, light
+          //create the perspective camera
+          var ratio = window.innerWidth/window.innerHeight;
+          var camera = new THREE.PerspectiveCamera(75,ratio,0.1,3000);
+  		//set camera position()
+          var pos = new THREE.Vector3(15,-15,-5);
+          camera.position.set(pos.x,pos.y,pos.z);
+          var dir = new THREE.Vector3(0,1,0);
+          camera.lookAt(dir.x,dir.y,dir.z);
+          camera.up.set( 0, 0, -1 );
+
+          var moveForward = false;
+          var moveLeft = false;
+          var moveBackward = false;
+          var moveRight = false;
+
+          const clock = new THREE.Clock();
+
+          var speed = 5;
+          var angle = 0;
+          //final update loop
+          var MyUpdateLoop = function ( ){
+              var delta = clock.getDelta();
+              if(moveLeft == true){
+                  angle+=speed*delta;
+                  dir.x = Math.sin(angle);
+                  dir.y = Math.cos(angle);
+                  dir.normalize();
+              }
+              if(moveRight == true){
+                  angle-=speed*delta;
+                  dir.x = Math.sin(angle);
+                  dir.y = Math.cos(angle);
+                  dir.normalize();
+              }
+              if(moveForward == true){
+                  pos.y+=dir.y*speed*delta;
+                  //pos.y+=dir.y*speed*delta;
+                  pos.x+=dir.x*speed*delta;
+              }
+              if(moveBackward == true){
+                  pos.y-=dir.y*speed*delta;
+                  //pos.y+=dir.y*speed*delta;
+                  pos.x-=dir.x*speed*delta;
+              }
+              //recompute direction
+              //control.update();
+              //call the render with the scene and the camera
+              renderer.render(scene,camera);
+              //finally perform a recoursive call to update again
+              //this must be called because the mouse change the camera position
+              camera.position.set(pos.x,pos.y,pos.z);
+              camera.lookAt(pos.x+dir.x,pos.y+dir.y,pos.z+dir.z);
+              camera.updateProjectionMatrix();
+              requestAnimationFrame(MyUpdateLoop);
+          };
+          requestAnimationFrame(MyUpdateLoop);
+*/
+
       /*var raycaster = new THREE.Raycaster();
 
       var selectedObj = false;
